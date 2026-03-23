52b80b7adddf727ecbb2dd2c97baf59adca90364
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-03-23 21:37:48 +0700

    Module Path trong Node.js

--- index.mjs
+++ index.mjs
@@ -6,10 +6,18 @@ import moment from "moment";
 
 // console.log(moment().format('DD/MM/YYYY HH:mm:ss'));
 
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+
+// console.log(path.basename(__filename));
+
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-
-console.log(path.basename(__filename));
+const __dirname = path.dirname(__filename);
+console.log(__filename);
+console.log(__dirname);
 
