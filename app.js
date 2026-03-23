196c6652bbce12a7e57b0a548685a47a17ebf457
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-03-23 21:14:11 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// const  { add, substract } = require("./utils/math");
+
+// console.log(add(10, 5));
+// console.log((substract(2, 8)));
+
+// console.log(__dirname);
+
+// const moment = require('moment');
+
+// console.log(moment().format('DD/MM/YYYY HH:mm:ss'));
+
+const path = require('path');
+
+console.log(path.basename(__filename));
+
