2db9515021224ead01b69d1af721e694ae2982c8
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-04 17:03:23 +0700

    update code

--- express-typescript/src/consumers/email.consumer.ts
+++ express-typescript/src/consumers/email.consumer.ts
@@ -5,20 +5,27 @@ const EX = "order_exchange";
 
 const emailConsumer = {
   async setup() {
+    // 1. Lấy instance RabbitMQ client
     const rabittmq = rabbitmqClient.getInstance();
+
+    // 2. Tạo/lấy channel
     rabittmq.getOrCreateChannel(
       "EMAIL_CHANNEL_CONSUMER",
       async (channel: ConfirmChannel) => {
+        // 3. Khai báo exchange
         await channel.assertExchange(EX, "direct", {
           durable: true,
         });
 
-        // Tạo queue tạm thời, xóa khi ngắt kết nối
+        // 4. Tạo queue riêng cho service email
         const queue = await channel.assertQueue("email_queue", {
           exclusive: true,
         });
 
+        // 5. Bind queue vào exchange
         await channel.bindQueue(queue.queue, EX, "order.created");
+
+        // 6. Consume message từ queue => Đăng ký lắng nghe message từ email_queue.
         channel.consume(queue.queue, (msg) => {
           if (msg) {
             const order = JSON.parse(msg.content.toString());
