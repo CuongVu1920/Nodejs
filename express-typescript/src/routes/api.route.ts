8076c80475765195759fbca37091e95ee7a7bff1
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-29 09:17:06 +0700

    Triển khai code RESTful API quản lý Users

--- express-typescript/src/routes/api.route.ts
+++ express-typescript/src/routes/api.route.ts
@@ -1,21 +1,8 @@
 import { Router } from "express";
-import { authMiddleware } from "../middlewares/auth.middleware";
-import { validate } from "../middlewares/validate.middleware";
-import { apiAuthController } from "../controllers/api/auth.controller";
-import { loginSchema, registerSchema } from "../validators/auth.validator";
-import { apiUserController } from "../controllers/api/user.controller";
+import apiV1Routes from "./api/v1";
 
 const router: Router = Router();
 
-router.post("/auth/login", validate(loginSchema), apiAuthController.login);
-router.get("/auth/me", authMiddleware, apiAuthController.profile);
-router.post("/auth/logout", authMiddleware, apiAuthController.logout);
-router.post("/auth/refresh-token", apiAuthController.refreshToken);
-
-router.get("/users", apiUserController.index);
-router.get("/users/:id", apiUserController.find);
-router.post("/users", validate(registerSchema), apiUserController.create);
-router.put("/users/:id", apiUserController.update);
-router.delete("/users/:id", apiUserController.delete);
+router.use("/v1", apiV1Routes);
 
 export default router;
---
+++ express-typescript/src/routes/api/v1.ts
@@ -0,0 +1,21 @@
+import { loginSchema, registerSchema } from "../../validators/auth.validator";
+import { Router } from "express";
+import { apiAuthController } from "../../controllers/api/auth.controller";
+import { apiUserController } from "../../controllers/api/user.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import { validate } from "../../middlewares/validate.middleware";
+
+const router: Router = Router();
+
+router.post("/auth/login", validate(loginSchema), apiAuthController.login);
+router.get("/auth/me", authMiddleware, apiAuthController.profile);
+router.post("/auth/logout", authMiddleware, apiAuthController.logout);
+router.post("/auth/refresh-token", apiAuthController.refreshToken);
+
+router.get("/users", apiUserController.index);
+router.get("/users/:id", apiUserController.find);
+router.post("/users", validate(registerSchema), apiUserController.create);
+router.put("/users/:id", apiUserController.update);
+router.delete("/users/:id", apiUserController.delete);
+
+export default router;
